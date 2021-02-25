677aef3d783a79f97105434f68795bad93636c89
Paweł <55764496+pdawic@users.noreply.github.com>
2021-02-25 13:59:46 +0100

    Add files via upload
    update sources and readme.md

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
 from sources import *
 
-print(covidData)
+ProcessingData().refresh_world_data()
+ProcessingData().show_world_covid_data()
 
+ProcessingData().refresh_polish_data()
+ProcessingData().show_polish_covid_data()
