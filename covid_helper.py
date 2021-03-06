8b12b92acfffd0a00038de2d3f2148b004f12689
Piotr <piotreqw@gmail.com>
2021-03-06 15:08:36 +0100

    Po zleanowaniu kodu

--- covid_helper.py
+++ covid_helper.py
@@ -9,51 +9,56 @@ import seaborn as sns
 
 base_color = sns.color_palette()[0]
 
+class RequestData:
+    def __init__(self, id: int, description: str, year: int, filename: str)->None:
+        self.id = id
+        self.description = description
+        self.year = year
+        self.filename = filename
+
+def _get_gus_data_from_all_pages(url: str, total_records: int):
+
+    max_records_per_page = 100
+    pages = int(total_records / max_records_per_page)
+    GUS_data = {}
+
+    for page in range(0, pages + 1):
+        url_pages = url + f'&page={page}&page-size=100'
+        req = requests.get(url_pages, headers={'X-ClientId': '38dfdca8-de37-41fc-44ab-08d8830c4874'})
+        data = req.json()
+
+        for elem in data['results']:
+            GUS_data[elem['id']] = [elem['name'], elem['values'][0]['val']]
+
+    return GUS_data
+
 def save_GUS_data(path):
     gus_path = path + '/' + 'gus_data'
 
-    # create a subfolder for geo data
     if not os.path.exists(gus_path):
         os.makedirs(gus_path)
 
-    # Chosen Variables:
-    # 72305 - liczba mieszkancow
-    # 746289 - mediana wieku ludnosci
-    # 1601437 - liczba boisk futbolowych - tylko 2018
-    # 1241 - liczba muzeow
-    # 64428 - przecietne wynagrodzenie brutto
-
-    variables = {
-        72305: ['liczba mieszkancow', 2019, 'Mieszkancy'],
-        746289: ['mediana wieku ludnosci',2019, 'Mediana'],
-        1601437: ['liczba boisk futbolowych', 2018, 'Boiska'],
-        1241: ['liczba muzeow', 2019, 'Muzea'],
-        64428: ['przecietne wynagrodzenie brutto', 2019, 'Wynagrodzenie']
-    }
+    gus_variables= [
+        RequestData(72305, 'liczba mieszkancow', 2019, 'Mieszkancy'),
+        RequestData(746289, 'mediana wieku ludnosci', 2019, 'Mediana'),
+        RequestData(1601437, 'liczba boisk futbolowych', 2018, 'Boiska'),
+        RequestData(1241, 'liczba muzeow', 2019, 'Muzea'),
+        RequestData(64428, 'przecietne wynagrodzenie brutto', 2019, 'Wynagrodzenie')
+    ]
 
     unit_level = 5  # poziom agregacji 5 - powiaty
 
-    for key, item in variables.items():
+    for gus_variable in gus_variables:
 
-        url = f'https://bdl.stat.gov.pl/api/v1/data/by-variable/{key}?unit-level={unit_level}&year={item[1]}'
+        url = f'https://bdl.stat.gov.pl/api/v1/data/by-variable/{gus_variable.id}?unit-level={unit_level}&year={gus_variable.year}'
         req = requests.get(url)
         data = req.json()
-
         total_records = data['totalRecords']
-        pages = int(total_records / 100)
 
-        GUS_data = {}
+        GUS_data = _get_gus_data_from_all_pages(url, total_records)
 
-        for page in range(0, pages + 1):
-            url_pages = url + f'&page={page}&page-size=100'
-            req = requests.get(url_pages, headers={'X-ClientId': '38dfdca8-de37-41fc-44ab-08d8830c4874'})
-            data = req.json()
-
-            for elem in data['results']:
-                GUS_data[elem['id']] = [elem['name'], elem['values'][0]['val']]
-                #print("{}: {} - {}".format(elem['id'], elem['name'], elem['values'][0]['val']))
-        df_GUS_data = pd.DataFrame(data=GUS_data, index=['Location', item[0]]).transpose()
-        df_GUS_data.to_csv(gus_path + '/' + item[2])
+        df_GUS_data = pd.DataFrame(data=GUS_data, index=['Location', gus_variable.description]).transpose()
+        df_GUS_data.to_csv(gus_path + '/' + gus_variable.filename)
 
 def save_covid_data(path):
     # link to the archived data per 'powiat'
