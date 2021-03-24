bfb1b2be86d60789affd2144442138d1a38c72e1
Paweł <55764496+pdawic@users.noreply.github.com>
2021-03-24 20:45:10 +0100

    Add validate_powiat
    Add validate_powiat

--- covid_main.py
+++ covid_main.py
@@ -1,7 +1,7 @@
 import covid_helper
 from SQL_DB import *
 import datetime
-
+import pandas as pd
 
 def validate_format(inp):
     try:
@@ -11,6 +11,25 @@ def validate_format(inp):
        inp = input()
        validate_format(inp)
 
+
+def validate_powiat(powiat):
+
+        df = pd.read_csv('./powiaty.csv')
+        powiat_list = df['powiat_miasto'].values
+
+        if powiat in powiat_list:
+            print('podałeś:', powiat)
+            return powiat
+        else:
+            print('Nie ten powiat, podaj go z poniższej listy:')
+            for element in powiat_list:
+                print(element)
+            powiat = input("podaj powiat: ")
+            validate_powiat(powiat)
+
+
+# powiat_input = input("podaj powiat: ")
+# validate_powiat(powiat_input)
 
 def main():
     print("Witaj w COVID APP")
@@ -52,6 +71,7 @@ def main():
             mydb.commit()
         elif var == 4:
             powiat = input('Podaj powiat\n')
+            # powiat = validate_powiat(powiat)
             covid_helper.plot(powiat, date_start, date_end)
             plot_tmp = ('Wykres dla: ' + str(powiat) + 'w datach od: ' + str(date_start) + 'do: ' +str(date_end))
             mycursor.execute("INSERT INTO logs (log) VALUES ('" + plot_tmp + "')")
